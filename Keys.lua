102096c20d473a912c4a673de463e9010953a792
deluxebadgame <59702790+deluxebadgame@users.noreply.github.com>
2023-07-29 01:30:46 +0300

    Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,4 +1,4 @@
 return {
   ["German11Lowik"] = {Key = "BubbleGum1", deported = false};
-  ["Hellower"] = {Key = "Qwerty", deported = true}
+  ["Hellower"] = {Key = "Qwerty", deported = true, deported_desc = "Нарушение ОДП. 1.5"}
 }
