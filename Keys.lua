3373f577c17fefd1a3c118dfe41befe12d5bb7f6
deluxebadgame <59702790+deluxebadgame@users.noreply.github.com>
2023-07-30 00:55:45 +0300

    Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,10 +1,15 @@
 return { --Rank: 1 участник, 2 ПНУ, 3 Дисциплинарный персонал, 4 Контрольный администратор.
   ["acc_base"] = {Key = "RVE11", deported = false, deported_desc = "", tokens = 0, acc_ver = "Alpha_23", rank = 4};
   ["German11Lowik"] = {Key = "PRm52", deported = false, deported_desc = "", tokens = 20, rank = 3};
-  ["Hellower"] = {Key = "Qwerty", deported = false, deported_desc = "", tokens = 10000, rank = 3};
-  ["Hellower2"] = {Key = "Qwerty", deported = true, deported_desc = "Нарушение ОДП. 1.5", rank = 3};
+  ["HELLOWER"] = {Key = "jpr55", deported = false, deported_desc = "", tokens = 10000, rank = 3};
+  ["Hellower2"] = {Key = "Qwerty5", deported = true, deported_desc = "Нарушение ОДП. 1.5", rank = 3};
   ["0880"] = {Key = "Mrks12", deported = false, deported_desc = "", tokens = 0, rank = 4};
   ["The_Mimic"] = {Key = "Wj2", deported = false, deported_desc = "", tokens = 0, rank = 1};
   ["Bobrilla"] = {Key = "cmM1", deported = false, deported_desc = "", tokens = 5, rank = 1};
-  ["1"] = {Key = "1", deported = false, deported_desc = "", tokens = 0, rank = 4}
+  ["Artem1337"] = {Key = "7Dkyr", deported = false, deported_desc = "", tokens = 0, rank = 1};
+  ["coolboylolYT"] = {Key = "F6kn2", deported = false, deported_desc = "", tokens = 0, rank = 1};
+  ["Zenitsu"] = {Key = "krb0", deported = false, deported_desc = "", tokens = 0, rank = 1};
+  ["Pomidorka"] = {Key = "aPd09", deported = false, deported_desc = "", tokens = 0, rank = 1};
+  ["fat"] = {Key = "PJE6", deported = false, deported_desc = "", tokens = 0, rank = 1};
+  ["Kakao"] = {Key = "4eQs", deported = false, deported_desc = "", tokens = 0, rank = 1}
 }
