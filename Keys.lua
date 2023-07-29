63355e921c5d569c367a87e9d139bd76cfa4a6cb
deluxebadgame <59702790+deluxebadgame@users.noreply.github.com>
2023-07-29 19:47:01 +0300

    Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,7 +1,7 @@
 return { --Rank: 1 участник, 2 ПНУ-персонал, 3 Дисциплинарный персонал, 4 Контрольный администратор.
   ["German11Lowik"] = {Key = "PRm52", deported = false, deported_desc = "", tokens = 20, rank = 3};
   ["Hellower"] = {Key = "Qwerty", deported = true, deported_desc = "Нарушение ОДП. 1.5", tokens = 0, rank = 3};
-  ["0880"] = {Key = "RobloxPro25", deported = false, deported_desc = "", tokens = 0, rank = 4};
+  ["0880"] = {Key = "Mrks12", deported = false, deported_desc = "", tokens = 0, rank = 4};
   ["The_Mimic"] = {Key = "Wj2", deported = false, deported_desc = "", tokens = 0, rank = 1};
   ["1"] = {Key = "1", deported = false, deported_desc = "", tokens = 0, rank = 4}
 }
