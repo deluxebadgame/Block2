c8b0d78957558c11fe8026caf601f3c493ce138e
deluxebadgame <59702790+deluxebadgame@users.noreply.github.com>
2023-07-30 13:00:04 +0300

    Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -2,7 +2,7 @@ return { --Rank: 1 участник, 2 ПНУ, 3 Дисциплинарный п
   ["acc_base"] = {Key = "RVE11", deported = false, deported_desc = "", tokens = 0, acc_ver = "Alpha_23", rank = 4};
   ["German11Lowik"] = {Key = "PRm52", deported = false, deported_desc = "", tokens = 20, rank = 3};
   ["HELLOWER"] = {Key = "jpr55", deported = false, deported_desc = "", tokens = 10000, rank = 3};
-  ["Hellower2"] = {Key = "Qwerty5", deported = true, deported_desc = "Нарушение ОДП. 1.5", tokens = 10000, rank = 3};
+  ["Hellower2"] = {Key = "Qwerty5", deported = true, deported_desc = "Нарушение ОДП. 1.5 / До 30.07.23", tokens = 10000, rank = 3};
   ["0880"] = {Key = "Mrks12", deported = false, deported_desc = "", tokens = 0, rank = 4};
   ["The_Mimic"] = {Key = "Wj2", deported = false, deported_desc = "", tokens = 0, rank = 1};
   ["Bobrilla"] = {Key = "cmM1", deported = false, deported_desc = "", tokens = 5, rank = 1};
